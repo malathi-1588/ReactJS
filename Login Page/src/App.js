1c5963a235357078364e3cad139b131bb9558c0f
Malathi M <116137959+malathi-1588@users.noreply.github.com>
2023-11-05 10:13:17 +0530

    Add files via upload

---
+++ Login Page/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import LoginPage from './LoginPage';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>LoginPage</h1>
+        <LoginPage/>
+      </header>
+    </div>
+  );
+};
+
+export default App;
